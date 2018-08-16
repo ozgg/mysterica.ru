f73fc92e63ae14cafe847c89795759a75bc06f3d
Maxim Khan-Magomedov <maxim.km@gmail.com>
2018-08-17 02:03:29 +0300

    Added sitemap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sitemap.xml' => 'sitemap#show', defaults: { format: :xml }
+
   scope '(:locale)', constraints: { locale: /ru|en/ } do
     root 'index#index'
   end
